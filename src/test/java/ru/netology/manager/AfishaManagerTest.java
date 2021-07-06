a43afa3c43e758681f1e97771e6530702afe789e
Ivan Rubinov <lrubinov@yandex.ru>
2021-07-06 12:34:19 +0300

    Layers2

--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -1,6 +1,7 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import ru.netology.domain.Movie;
@@ -8,19 +9,23 @@ import ru.netology.repository.AfishaRepository;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
+
+@ExtendWith(MockitoExtension.class)
 public class AfishaManagerTest {
-    //@Mock
-    //private AfishaRepository repository;
-    AfishaRepository repository = new AfishaRepository();
-    //    @InjectMocks
-//    private AfishaManager manager;
-    Movie first = new Movie("first", "imageUrl1", "name1", "genre1");
-    Movie second = new Movie("second", "imageUrl2", "name2", "genre2");
-    Movie third = new Movie("third", "imageUrl3", "name3", "genre3");
+    @Mock
+    private AfishaRepository repository;
+    @InjectMocks
+    private AfishaManager manager;
+    private Movie first = new Movie("first", "imageUrl1", "name1", "genre1");
+    private Movie second = new Movie("second", "imageUrl2", "name2", "genre2");
+    private Movie third = new Movie("third", "imageUrl3", "name3", "genre3");
 
     @Test
     public void shouldRemoveIfExists() {
+        AfishaRepository repository = new AfishaRepository();
         AfishaManager manager = new AfishaManager(repository);
         manager.add(first);
         manager.add(second);
@@ -35,6 +40,7 @@ public class AfishaManagerTest {
     }
     @Test
     public void shouldRemoveIfNotExists() {
+        AfishaRepository repository = new AfishaRepository();
         AfishaManager manager = new AfishaManager(repository);
         manager.add(first);
         manager.add(second);
